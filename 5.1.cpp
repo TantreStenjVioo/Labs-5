38c690c0eda0abe7b1832c7cf47f914b8bfe4151
TantreStenjVioo <96138379+TantreStenjVioo@users.noreply.github.com>
2021-12-16 03:38:47 +0300

    Update 5.1.cpp

--- 5.1.cpp
+++ 5.1.cpp
@@ -3,9 +3,9 @@
 
 int main() 
 {
-    double x1, y1, x2, y2, result;
+    double x1, y1, x2, y2, result; // Задаём координаты точек
     scanf("%lf %lf %lf %lf", &x1, &y1, &x2, &y2);
-    result = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-    printf("%.2lf", result);
+    result = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)); // По теореме Пифагора вычисляем расстояние между точками
+    printf("%.2lf", result); // Выводим длину
     return 0;
 }
