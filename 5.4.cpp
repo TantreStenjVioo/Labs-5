e9f5bb106980407d855d1ab2f3a2a61a1d632d0d
TantreStenjVioo <96138379+TantreStenjVioo@users.noreply.github.com>
2021-12-16 03:50:37 +0300

    Update 5.4.cpp

--- 5.4.cpp
+++ 5.4.cpp
@@ -1,35 +1,22 @@
 #include <stdio.h>
 #include <math.h>
- 
+
 int main(void)
 {
-    float x1,y1;
+    float x1, y1; // Задаём координаты точек
     printf("x1:");
     scanf ("%f", &x1);
     printf("y1:");
     scanf ("%f", &y1);
- 
-    float x2,y2;
+
+    float x2,y2; // Задаём координаты точек
     printf("x2:");
     scanf ("%f", &x2);
     printf("y2:");
     scanf ("%f", &y2);
- 
-    float x3,y3;
-    printf("x3:");
-    scanf ("%f", &x3);
-    printf("y3:");
-    scanf ("%f", &y3);
- 
-     float a,b,c;
-    a=sqrt(pow(x1-x2,2)+pow(y1-y2,2));
-    b=sqrt(pow(x2-x3,2)+pow(y2-y3,2));
-    c=sqrt(pow(x3-x1,2)+pow(y3-y1,2));
- 
-    float S,p;
-    p=(a+b+c)/2;
-    S=sqrt(p*(p-a)*(p-b)*(p-c));
-    printf("S:%f\n",S);
- 
+
+    printf("P:%f\n", 2 * (abs(x1 - x2) + abs(y1 - y2))); // Вычисляем периметр и выводим полученные данные
+
+    printf("S:%f\n", abs(x1 - x2) * abs(y1 - y2)); // Вычисляем площадь и выводим полученные данные
     return 0;
 }
