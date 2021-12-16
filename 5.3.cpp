dba7a261574453f26c75cf045c81aa98b4c145b7
TantreStenjVioo <96138379+TantreStenjVioo@users.noreply.github.com>
2021-12-16 03:44:11 +0300

    Update 5.3.cpp

--- 5.3.cpp
+++ 5.3.cpp
@@ -3,12 +3,12 @@
  
 int main()
 {
-    int A, B, C;
+    int A, B, C; // Задаём точки
     printf("Input A, B and C (through a space): ");
     scanf("%i %i %i", &A, &B, &C);
-    int AC = abs(C - A);
-    int BC = abs(C - B);
+    int AC = abs(C - A); // Вычисляем длину отрезков
+    int BC = abs(B - C);
     printf("AC = %i; BC = %i\n", AC, BC);
- 	printf("AC + BC = %i\n", AC * BC);
+ 	  printf("AC * BC = %i\n", AC * BC); // Умножаем длины отрезков и выводим полученный результат
     return 0;
 }
